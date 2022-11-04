49febceddab9c84683d2efb7db08f3d4c0d7e8b6
user-Maxim-name <100277248+user-Maxim-name@users.noreply.github.com>
2022-11-04 12:36:05 +0200

    Update script.js

--- PR4/task/script.js
+++ PR4/task/script.js
@@ -41,7 +41,9 @@ function displayLocation(position) {
   if (map == null) {
     map = L.map("map").setView([latitude, longitude], 13);
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
-    L.marker([latitude, longitude]).addTo(map);
+    L.marker([latitude, longitude]).addTo(map).bindPopup(
+        `${new Date().toString()} <br /> Latiude: ${latitude} <br />Longitude ${longitude}`
+      );
   } else {
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
     L.marker([latitude, longitude])
